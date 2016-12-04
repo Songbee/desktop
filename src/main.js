d46214bc59b6dc9eac75a235c4bf8b760a1b6308
Alexander Pushkov <hi@ale.rocks>
2016-12-04 18:12:43 +0300

    Close to tray (and keep playing)
    #nowplaying CAZZETTE — Beam me up (radio edit)

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import electron, { app, BrowserWindow } from "electron";
+import electron, { app, BrowserWindow, Menu, Tray } from "electron";
 import path from "path";
 import url from "url";
 
 import Manager from "./backend";
 global.backend = new Manager();
 
-let mainWindow; // keep a reference
+let mainWindow, tray; // keep a reference
 
 const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
   // Someone tried to run a second instance, we should focus our window.
   if (mainWindow) {
+    if (!mainWindow.isVisible()) mainWindow.show();
     if (mainWindow.isMinimized()) mainWindow.restore();
     mainWindow.focus();
   }
@@ -17,16 +18,15 @@ const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
 
 if (shouldQuit) app.quit();
 
+
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     show: false,
-    autoHideMenuBar: true
+    autoHideMenuBar: true,
   });
-  mainWindow.once("ready-to-show", () => {
-    mainWindow.show();
-  });
+  mainWindow.once("ready-to-show", () => mainWindow.show());
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, "frontend/index.html"),
@@ -34,20 +34,29 @@ function createWindow() {
     slashes: true,
   }));
 
-  mainWindow.on("closed", () => {
-    // Dereference the window object.
-    mainWindow = null;
+  mainWindow.on("close", (event) => {
+    event.preventDefault();
+    mainWindow.hide();
   });
 }
 
 app.on("ready", () => {
+  tray = new Tray(path.join(__dirname, "res/icon.png"));
+  const trayMenu = Menu.buildFromTemplate([
+    { role: 'quit' },
+  ]);
+  tray.setToolTip("~Songbee!!!!!!!");
+  tray.setContextMenu(trayMenu);
+
   createWindow();
+
+  tray.on("click", () => {
+    mainWindow.show();
+  });
 });
 
 app.on("window-all-closed", () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") app.quit();
+  // No-op. We listen for this event so that Electron doesn't exit.
 });
 
 app.on("activate", () => {
