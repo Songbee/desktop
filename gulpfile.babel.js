99919436eac4c8896331faa52e012c7552c44923
Alexander Pushkov <hi@ale.rocks>
2017-02-11 19:36:27 +0300

    Add task `clean` (rm ./build)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,7 @@ const filter = require("gulp-filter");
 const babel = require("gulp-babel");
 const sourcemaps = require("gulp-sourcemaps");
 const yarn = require("gulp-yarn");
+const del = require("del");
 
 gulp.task("build", () => {
   const f = {
@@ -22,4 +23,8 @@ gulp.task("vendor", function() {
   return gulp.src(["./package.json", "./yarn.lock"])
     .pipe(gulp.dest("./dist"))
     .pipe(yarn({ production: true }));
+});
+
+gulp.task("clean", function() {
+    return del(["./build"]);
 });
